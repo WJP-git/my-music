6ffb0a0a660ebff4c9fb5f9ada24ab1cc705c4ba
wjp <416395274@qq.com>
2020-08-29 17:49:39 +0800

    首页

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Tabbar></Tabbar>
+    <router-view></router-view>
   </div>
 </template>
+
 <script>
-import Header from './components/Header'
+import Header from 'components/Header'
+import Tabbar from 'components/Tabbar'
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tabbar
+
+  },
+  methods: {
+
+  },
+  created () {
+
   }
 }
 
 </script>
 <style lang="scss">
+@import "./assets/css/variable.scss";
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Recommend = () => import('../views/Recommend')
+const Singer = () => import('../views/Singer')
+const Rank = () => import('../views/Rank')
+const Search = () => import('../views/Search')
+
 Vue.use(VueRouter)
 
 const routes = [
-
+  { path: '/', redirect: '/recommend' },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/rank',
+    component: Rank
+  },
+  {
+    path: '/search',
+    component: Search
+  }
 ]
 
 const router = new VueRouter({
